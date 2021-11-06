9cf1fa716820f3a2317248c6afcdeb60ae4467c7
Rebecca <rebeccaignacio@outlook.com>
2021-11-06 00:56:52 -0300

    finalizado - hero background

---
+++ src/components/HeroSection/index.js
@@ -0,0 +1,51 @@
+import React, {useState} from 'react'
+import {HeroContainer, HeroBg, VideoBg, HeroContent, HeroH1, HeroP, HeroBtnWrapper, ArrowForward, ArrowRight} from './HeroElements'
+import Video from '../../videos/video.mp4'
+import { Button } from '../ButtonElement'
+
+
+const HeroSection = () => {
+
+    const [hover, setHover] = useState(false)
+
+    const onHover = () => {
+        setHover(!hover)
+    }
+
+    return (
+        <HeroContainer>
+            <HeroBg>
+                <VideoBg 
+                    autoPlay 
+                    loop 
+                    muted 
+                    src={Video}
+                    type="video/mp4"
+                />
+            </HeroBg>
+            <HeroContent>
+                <HeroH1>
+                    Virtual Banking Made Easy
+                </HeroH1>
+                <HeroP>
+                    Sign up for a free account and start using your 
+                    bank account in minutes.
+                </HeroP>
+                <HeroBtnWrapper>
+                    <Button to="signup" 
+                            onMouseEnter={onHover} 
+                            onMouseLeave={onHover}
+                            primary="true"
+                            dark="true"
+
+                    > 
+                        Get started 
+                        {hover ? <ArrowForward/> : <ArrowRight/>}
+                    </Button>
+                </HeroBtnWrapper>
+            </HeroContent>
+        </HeroContainer>
+    )
+}
+
+export default HeroSection

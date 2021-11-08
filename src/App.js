9a68d775648ffcadc18a9ae6e76819ecd079afe8
Rebecca <rebeccaignacio@outlook.com>
2021-11-08 16:18:23 -0300

    fix router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import './App.css'
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import Home from './pages';
+import Home from './pages/index';
 import SigninPage from './pages/signin'
 
 function App() {
   return (
     <Router>
-    <Routes>
-      <Route path="/" component={<Home />}>
-        <Route path="/signin" component={<SigninPage />} />
-      </Route>
-    </Routes>
+      <Routes>
+        <Route exact path="/" element={<Home />} component={Home} />
+        <Route exact path="/signin" element={<SigninPage/>} component={SigninPage} />
+      </Routes>
     <Home/>
   </Router>
   );
